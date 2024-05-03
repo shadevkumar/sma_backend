4e60501220834448b210f508503ec27d1bfc31da
shadevkumar <shadevsrk261@gmail.com>
2024-05-04 02:42:58 +0530

    HttpCode decorator added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,6 +4,7 @@ import {
   Body,
   HttpException,
   HttpStatus,
+  HttpCode,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
@@ -12,16 +13,19 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
   async signup(@Body() createUserDto: any) {
     return this.authService.signup(createUserDto);
   }
 
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   async login(@Body() loginUserDto: any): Promise<any> {
     return this.authService.login(loginUserDto);
   }
 
   @Post('refresh')
+  @HttpCode(HttpStatus.OK)
   async refreshTokens(@Body() body: { refreshToken: string }) {
     try {
       const newTokens = await this.authService.refreshTokens(body.refreshToken);
@@ -32,6 +36,7 @@ export class AuthController {
   }
 
   @Post('logout')
+  @HttpCode(HttpStatus.OK)
   async logout(@Body() body: { userId: string }) {
     await this.authService.logout(body.userId);
     return { message: 'Logged out successfully' };
--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://sma-patilkaki.vercel.app', // Specify the frontend domain
+    origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
   });
   await app.listen(4001, () => {
     console.log('Server listening on port 4001!');
