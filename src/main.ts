21935896769d5ebf5fd3aaa1493744c62ceed836
shadevkumar <shadevsrk261@gmail.com>
2024-05-04 02:22:11 +0530

    CORS settings in deployment

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://sma-patilkaki.vercel.app', // Specify the frontend domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   await app.listen(4001, () => {
     console.log('Server listening on port 4001!');
   });
