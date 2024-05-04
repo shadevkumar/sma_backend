3a9260fdfb14e06254de8a37bd6626fa350edb51
shadevkumar <shadevsrk261@gmail.com>
2024-05-04 08:18:36 +0530

    Update CORS settings

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: '*', 
+    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,
   });
-  await app.listen(4001, () => {
-    console.log('Server listening on port 4001!');
+  await app.listen(process.env.PORT || 4001, () => {
+    console.log(`Server listening on port ${process.env.PORT || 4001}!`);
   });
 }
 bootstrap();
